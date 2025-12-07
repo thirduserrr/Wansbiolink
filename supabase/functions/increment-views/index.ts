5f8c3070bc0f8989a695c53f3c706e2e66e4a115
Wan <wansbussiness@gmail.com>
2025-12-06 23:27:10 -0500

    Update index.ts

--- supabase/functions/increment-views/index.ts
+++ supabase/functions/increment-views/index.ts
@@ -1,9 +1,9 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
-};
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
