04f2fa3bbb1b67f6246316254233e85a96896700
Wan <wansbussiness@gmail.com>
2025-12-06 23:41:12 -0500

    Update index.ts

--- supabase/functions/increment-views/index.ts
+++ supabase/functions/increment-views/index.ts
@@ -1,14 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY missing in .env");
+  throw new Error("VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY missing");
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 export async function incrementPageView() {
